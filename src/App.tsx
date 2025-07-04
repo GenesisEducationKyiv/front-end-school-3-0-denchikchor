f1c1d51762508875e045edd247327e6a8b7c6b65
denchikchor <59959537+denchikchor@users.noreply.github.com>
2025-07-04 13:36:32 +0300

    Added code splitting and lazy loading for forms in modal (v2)

---
+++ src/App.tsx
@@ -0,0 +1,48 @@
+import { lazy, Suspense } from "react";
+import { ToastContainer } from "react-toastify";
+import { useSelector } from "react-redux";
+import Header from "./components/Header/Header";
+import Preloader from "./components/Preloader/Preloader";
+import {
+  selectCreateModalOpen,
+  selectEditTrackId,
+} from "./features/ui/modalSlice";
+import TrackList from "./components/TrackList/TrackList";
+
+const TrackCreateModal = lazy(() =>
+  import("./components/TrackCreateModal/TrackCreateModal")
+);
+const TrackEditModal = lazy(() =>
+  import("./components/TrackEditModal/TrackEditModal")
+);
+
+function App() {
+  const isCreateOpen = useSelector(selectCreateModalOpen);
+  const isEditOpen = useSelector(selectEditTrackId);
+
+  return (
+    <>
+      <Header />
+
+      <main className="content">
+        <TrackList />
+      </main>
+
+      {isCreateOpen && (
+        <Suspense fallback={<Preloader />}>
+          <TrackCreateModal />
+        </Suspense>
+      )}
+
+      {isEditOpen && (
+        <Suspense fallback={<Preloader />}>
+          <TrackEditModal />
+        </Suspense>
+      )}
+
+      <ToastContainer position="top-right" autoClose={3000} />
+    </>
+  );
+}
+
+export default App;
