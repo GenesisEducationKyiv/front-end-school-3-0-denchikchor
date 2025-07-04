04f357ad2c05fc9f13b44b06fd40ecbee9f184d4
denchikchor <59959537+denchikchor@users.noreply.github.com>
2025-07-04 13:14:54 +0300

    Add .env file and environment variable support

--- src/apollo/client.ts
+++ src/apollo/client.ts
@@ -1,9 +1,10 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client';
 import createUploadLink from 'apollo-upload-client/createUploadLink.mjs';
+const apiUrl = import.meta.env.VITE_API_URL;
 
 export const client = new ApolloClient({
   link: createUploadLink({
-    uri: 'http://localhost:8000/graphql',
+    uri: apiUrl,
     credentials: 'include',
   }),
   cache: new InMemoryCache(),
