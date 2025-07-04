32de3c59559e9a07b3e472a7768dd45fad1b1295
denchikchor <59959537+denchikchor@users.noreply.github.com>
2025-07-04 13:00:27 +0300

    Added code splitting and lazy loading for forms in modal

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,46 @@
-import React, { useState } from "react";
-
-import TrackCreateModal from "./components/TrackCreateModal/TrackCreateModal";
-import TrackList from "./components/TrackList/TrackList";
-import TrackEditModal from "./components/TrackEditModal/TrackEditModal";
+import { lazy, Suspense } from "react";
 import { ToastContainer } from "react-toastify";
+import { useSelector } from "react-redux";
 import Header from "./components/Header/Header";
+import Preloader from "./components/Preloader/Preloader";
+import {
+  selectCreateModalOpen,
+  selectEditTrackId,
+} from "./features/ui/modalSlice";
+import TrackList from "./components/TrackList/TrackList";
 
+// Lazy-імпорти
+const TrackCreateModal = lazy(() =>
+  import("./components/TrackCreateModal/TrackCreateModal")
+);
+const TrackEditModal = lazy(() =>
+  import("./components/TrackEditModal/TrackEditModal")
+);
 
 function App() {
+  const isCreateOpen = useSelector(selectCreateModalOpen);
+  const isEditOpen = useSelector(selectEditTrackId);
 
   return (
     <>
       <Header />
+
       <main className="content">
         <TrackList />
       </main>
-        <TrackCreateModal />
-        <TrackEditModal />
+
+      {isCreateOpen && (
+        <Suspense fallback={<Preloader />}>
+          <TrackCreateModal />
+        </Suspense>
+      )}
+
+      {isEditOpen && (
+        <Suspense fallback={<Preloader />}>
+          <TrackEditModal />
+        </Suspense>
+      )}
+
       <ToastContainer position="top-right" autoClose={3000} />
     </>
   );
