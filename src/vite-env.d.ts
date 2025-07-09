f0c8875bb5212172ed0a51e9f014a91ea729be8e
denchikchor <59959537+denchikchor@users.noreply.github.com>
2025-07-09 13:45:44 +0300

    Use WebP images for improved performance

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -24,6 +24,11 @@ declare module "*.png" {
   export default value;
 }
 
+declare module "*.webp" {
+  const value: string;
+  export default value;
+}
+
 declare module "*.svg" {
   import * as React from "react";
   export const ReactComponent: React.FC<React.SVGProps<SVGSVGElement>>;
