66f66df4ff62be041c47eb2bfc82f96f9a2b07b6
denchikchor <59959537+denchikchor@users.noreply.github.com>
2025-07-18 12:35:31 +0300

    Revert "chore: add server to the repository"
    This reverts commit b760f9931fbc1255e4ba0af57366ea6a7b54578c.

--- src/graphql/constants.ts
+++
@@ -1,8 +0,0 @@
-export const allowedMimeTypes = [
-          "audio/mpeg",
-          "audio/wav",
-          "audio/mp3",
-          "audio/x-wav",
-        ];
-
-export const maxSize = 10 * 1024 * 1024; // 10MB
